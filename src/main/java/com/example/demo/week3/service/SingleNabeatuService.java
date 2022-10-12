69751852f6f7841445fac41564aaaefa6752fbb6
eisuke212 <eisukeb7dr@gmail.com>
2022-10-12 19:56:18 +0900

    Nabeatu

--- src/main/java/com/example/demo/week3/service/SingleNabeatuService.java
+++ src/main/java/com/example/demo/week3/service/SingleNabeatuService.java
@@ -18,7 +18,23 @@ public class SingleNabeatuService {
      * @param inputNumber 画面で入力した数字
      * @return flg アホになる('3を含む数字'または'3の倍数')かどうかの結果
      */
+	
     public boolean isAhoNumber(int inputNumber) {
- 
+    	boolean flg;
+    	
+    	if(inputNumber % 3 == 0) {
+    		flg = true;
+    	} else if (inputNumber % 10 == 3) {
+    		flg = true;
+    	} else if (inputNumber % 100 == 3) {
+    		flg = true;
+    	} else if (inputNumber % 1000 == 3) {
+    		flg = true;
+    	} else {
+    		flg = false;
+    	}
+    	return flg;
     }
+    //int flg = isAhoNumber;
+    //return flg;
 }
